24bf9698ae6aaacbd7ca4dd0f034a07778104935
Azu <azu.moreno.orozco@gmail.com>
2017-06-29 15:24:46 -0500

    Add files via upload

---
+++ caesar.py
@@ -0,0 +1,18 @@
+#caesar
+import sys
+
+n = int(raw_input().strip())
+s =raw_input().strip();
+new_s=[]
+k=int(raw_input().strip())
+
+azu=""
+for a in s:
+  if a.isalpha():
+    if a.islower():
+      azu= azu+chr((((ord(a)+k)-97)%26)+97)
+    else:
+      azu= azu+chr((((ord(a)+k)-65)%26)+65)
+  else:
+    azu= azu + a
+print azu
